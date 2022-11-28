dc79172af3693084fa59fa9df20332ba0da2ca4d
yh4752 <yh4752@naver.com>
2022-11-28 13:02:41 +0900

    modi access all cors

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ import corsOptions from "./config/corsOptions.js";
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "*", // 출처 허용 옵션
+    credential: "true", // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+  })
+);
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
