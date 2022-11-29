ee96774ed9192d2a52ef4dd290d2d0d4fde7af6e
yh4752 <yh4752@naver.com>
2022-11-29 17:00:02 +0900

    change origin url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
-  "https://beamish-zuccutto-c84c40.netlify.applogin#",
+  "https://beamish-zuccutto-c84c40.netlify.app",
 ];
 
 export default allowedOrigins;
