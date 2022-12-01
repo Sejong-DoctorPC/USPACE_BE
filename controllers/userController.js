c5df0b66632f57b8d8b564e866595f5703ca756f
yh4752 <yh4752@naver.com>
2022-12-01 11:56:03 +0900

    modi userCon

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,17 +2,12 @@ import User from "../models/User.js";
 import Sample from "../models/Sample.js";
 
 export const getJoin = async (req, res) => {
-  User.find({}, function (err, result) {
-    if (err) {
-      return console.log(err);
-    } else {
-      return res.json(result);
-    }
-  });
+  const users = await User.find({});
+  return res.send(users);
 };
 export const postJoin = async (req, res) => {
   const { user, pwd } = req.body;
-  console.log(user);
+  console.log(pwd);
   const exists = await User.exists({ $or: [{ user }, { pwd }] });
   if (exists) {
     return res
@@ -21,43 +16,15 @@ export const postJoin = async (req, res) => {
   }
 
   try {
-    const user = await User.create({
-      user,
-      pwd,
+    const newUser = await User.create({
+      user: user,
+      pwd: pwd,
     });
-
-    return res.status(201).json(user);
+    console.log("db 저장 완료~!!");
+    return res.status(201).json(newUser);
   } catch (error) {
     return res.status(400).json({ message: error._message });
   }
-};
-
-export const createSamples = async (req, res) => {
-  await Sample.create({
-    zone: "A1",
-    type: 2,
-    state: 1,
-  });
-
-  await Sample.create({
-    zone: "B2",
-    type: 3,
-    state: 0,
-  });
-
-  await Sample.create({
-    zone: "C1",
-    type: 1,
-    state: 1,
-  });
-
-  await Sample.create({
-    zone: "D5",
-    type: 1,
-    state: 0,
-  });
-
-  return;
 };
 
 export const getParking = async (req, res) => {
